8b604b7d22e333f6de4dfa6f38ef7573df174591
Josh Benner <josh@bennerweb.com>
2016-06-05 22:26:12 -0400

    Update to depend on extracted mudsling plugin packages.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email='josh@bennerweb.com',
 
     packages=find_packages(),
+    include_package_data=True,
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     install_requires=['mudsling', 'ldaptor'],
